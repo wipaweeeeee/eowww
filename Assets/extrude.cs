15cd0e5bbb45c83b9797000a98ba6027cf5d50b1
Wipawe Sirikolkarn <Wipawe.Sirikolkarn@frogdesign.com>
2018-04-23 18:12:38 -0400

    prefab instantiated

---
+++ Assets/extrude.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class extrude : MonoBehaviour {
+
+	public Transform prefab;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 10; i++) {
+			Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/extrude_2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class extrude_2 : MonoBehaviour {
+
+    public Transform brick;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(brick, new Vector3(0, 0, i), Quaternion.Euler(0, 180, 0));
+        }
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}

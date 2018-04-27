44a56e87969add9df46fb48e835afb60469370e7
Wipawe Sirikolkarn <Wipawe.Sirikolkarn@frogdesign.com>
2018-04-27 18:43:25 -0400

    tryna add vuforia

--- Assets/extrude_2.cs
+++ Assets/extrude_2.cs
@@ -9,29 +9,28 @@ public class extrude_2 : MonoBehaviour {
     public int numberOfObjects = 10;
     public float radius = 10f;
     float time = 1;
+    float distort = 1;
 
 
 	// Use this for initialization
 	void Start () {
-        //for (int i = 0; i < numberOfObjects; i++)
-        //{
-        //    float angle = (i * Mathf.PI / 3) / numberOfObjects;
-        //    Vector3 pos = new Vector3(Mathf.Cos(angle*10*Time.deltaTime), 0, Mathf.Sin(angle*10*Time.deltaTime)) * radius;
-        //    Instantiate(brick, pos, Quaternion.Euler(0, 180, 0));
-        //    //brickClone = Instantiate(brick, new Vector3(0, 0, i), Quaternion.Euler(0, 180, 0));
-        //}
-
+        //Instantiate(brick, new Vector3(0,0,0), Quaternion.Euler(0, 180, 0));
 	}
 
     void drawStuff() {
-        float angle = (time * Mathf.PI / 3) / numberOfObjects;
-        Debug.Log(angle);
-        Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(-angle)*Mathf.Cos(angle), Mathf.Sin(angle)) * radius;
+        float angle = (time * Mathf.PI / 2) / numberOfObjects;
+        brick.transform.localScale = new Vector3(distort, 1, 7);
+        Vector3 pos = new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle)) * radius;
         Instantiate(brick, pos, Quaternion.Euler(0, 180, 0));
     }
 	
 	//Update is called once per frame
 	void Update () {
+        if (distort > 2) {
+            distort -= 0.01f;
+        } else {
+            distort += 0.01f;
+        }
 
         time+=0.1f;
         if (time < 20)
